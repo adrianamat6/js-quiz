3a10ccb474bdca9853ccf97668557252e8d36c8a
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2025-12-05 20:26:50 +0100

    Añadido de botón para empezar el concurso de preguntas

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 
 
-let nombre = prompt('¿Cómo te llamas?')
-let respuesta_1 = prompt(nombre + ' '+ ', ¿cuál es la capital de Italia?')
+function start_quiz(){
+
+    let nombre = prompt('¿Cómo te llamas?')
+    let respuesta_1 = prompt(nombre + ' '+ ', ¿cuál es la capital de Italia?')
+    let cajaResultado = document.querySelector( '#contenedor_fallo_pregunta' );
 
 
-if (respuesta_1 === 'Roma'){
-    let respuesta_2 = prompt(nombre + ' '+ ', ¿en qué estación del año estamos?')
+    if (respuesta_1 === 'Roma'){
+        let respuesta_2 = prompt(nombre + ' '+ ', ¿en qué estación del año estamos?')
 
-    if (respuesta_2 ==='Otoño'){
-        let respuesta_3 = prompt(nombre + ' '+ '¿en qué continente está China?')
-        
-        if (respuesta_3 ==='Asia'){
-            let nodoTodoAciertos= document.querySelector( '#contenedor_fallo_pregunta' );
-            nodoTodoAciertos.innerHTML = '<h1> Felicidades ' + nombre + ', todas las respuestas son correctas</h1>';
+        if (respuesta_2 ==='Otoño'){
+            let respuesta_3 = prompt(nombre + ' '+ '¿en qué continente está China?')
+            
+            if (respuesta_3 ==='Asia'){
+                cajaResultado.innerHTML = '<h1> Felicidades ' + nombre + ', todas las respuestas son correctas</h1>';
+            }else{
+                cajaResultado.innerHTML = '<h1>' + nombre + ', fallaste, ¡vuelve a intentarlo!</h1>';
+            }
+
         }else{
-            let nodoContenedorFallo3 = document.querySelector( '#contenedor_fallo_pregunta' );
-            nodoContenedorFallo3.innerHTML = '<h1>' + nombre + ', fallaste, ¡vuelve a intentarlo!</h1>';
+            cajaResultado.innerHTML = '<h1>' + nombre + ', fallaste, ¡vuelve a intentarlo!</h1>';
         }
-
     }else{
-        let nodoContenedorFallo2 = document.querySelector( '#contenedor_fallo_pregunta' );
-        nodoContenedorFallo2.innerHTML = '<h1>' + nombre + ', fallaste, ¡vuelve a intentarlo!</h1>';
+        cajaResultado.innerHTML = '<h1>' + nombre + ', tienes que estudiar más</h1>';
     }
-}else{
-    let nodoContenedorFallo1 = document.querySelector( '#contenedor_fallo_pregunta' );
-    nodoContenedorFallo1.innerHTML = '<h1>' + nombre + ', tienes que estudiar más</h1>';
+
+
+
 }
-
-
-
-
