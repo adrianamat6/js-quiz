851db9c3e2ad634035b18f351e0bd421bb4aad4c
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2025-12-05 20:17:41 +0100

    Ejercicio acabado en cuanto a la logica

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 
 
 let nombre = prompt('¿Cómo te llamas?')
-let respuesta_1 = prompt(nombre + ' '+ ', ¿Cuál es la capital de Italia?')
+let respuesta_1 = prompt(nombre + ' '+ ', ¿cuál es la capital de Italia?')
 
 
 if (respuesta_1 === 'Roma'){
-    
+    let respuesta_2 = prompt(nombre + ' '+ ', ¿en qué estación del año estamos?')
+
+    if (respuesta_2 ==='Otoño'){
+        let respuesta_3 = prompt(nombre + ' '+ '¿en qué continente está China?')
+        
+        if (respuesta_3 ==='Asia'){
+            let nodoTodoAciertos= document.querySelector( '#contenedor_fallo_pregunta' );
+            nodoTodoAciertos.innerHTML = '<h1> Felicidades ' + nombre + ', todas las respuestas son correctas</h1>';
+        }else{
+            let nodoContenedorFallo3 = document.querySelector( '#contenedor_fallo_pregunta' );
+            nodoContenedorFallo3.innerHTML = '<h1>' + nombre + ', fallaste, ¡vuelve a intentarlo!</h1>';
+        }
+
+    }else{
+        let nodoContenedorFallo2 = document.querySelector( '#contenedor_fallo_pregunta' );
+        nodoContenedorFallo2.innerHTML = '<h1>' + nombre + ', fallaste, ¡vuelve a intentarlo!</h1>';
+    }
+}else{
+    let nodoContenedorFallo1 = document.querySelector( '#contenedor_fallo_pregunta' );
+    nodoContenedorFallo1.innerHTML = '<h1>' + nombre + ', tienes que estudiar más</h1>';
 }
+
 
 
 
